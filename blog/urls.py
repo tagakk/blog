dba812b9475b5b87778b6fccbd283680647a0310
taka <taka.intn07@gmail.com>
2020-03-08 20:48:03 +0900

    add post-detail

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post-list')
+    path('', views.post_list, name='post-list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
